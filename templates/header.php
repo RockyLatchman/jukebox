ebbf0b141d7a81e62231c6d13e17d50e871abafd
RockyLatchman <r.latchman3624@gmail.com>
2025-10-23 13:30:51 +0700

    HTML for header and footer templates

---
+++ templates/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+     <title>Jukebox</title>
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+     <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&family=Red+Rose:wght@300..700&display=swap" rel="stylesheet">
+     <link rel="stylesheet" href="./public/css/main.css">
+     <script src="./public/js/main.js"></script>
+ </head>
+ <body>

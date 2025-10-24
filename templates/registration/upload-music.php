b1d6e49c6ca5d8cdaadd9a76a813d03273f3492a
RockyLatchman <r.latchman3624@gmail.com>
2025-10-24 20:26:48 +0700

    URL Routing and HTML for upload music template

---
+++ templates/registration/upload-music.php
@@ -0,0 +1,29 @@
+<header>
+    <div class="logo">
+        <a href="http://localhost/jukebox"><img src="http://localhost/jukebox/public/images/music_note.png" alt="Image of music note">
+       <h2>Jukebox</h2></a>
+    </div>
+   <nav>
+     <menu>
+        <li><a href="http://localhost/jukebox/features">Features</a></li>
+        <li><a href="http://localhost/jukebox/music/collections">Play music</a></li>
+        <li><a href="http://localhost/jukebox/api">API</a></li>
+     </menu>
+     <menu>
+        <li><a href="http://localhost/jukebox/sign-in">Sign in</a></li>
+        <li><a href="http://localhost/jukebox/sign-up">Sign up</a></li>
+     </menu>
+   </nav>
+</header>
+<main>
+    <section>
+        <div>
+         <form method="POST" name="track-upload-form">
+            <h3>Add music</h3>
+            <p>Drop one or more of your songs below</p>
+            <div class="track-uploads"><p>Drop tracks here</p></div>
+            <input type="submit" name="track-upload" value="Save genres">
+         </form>
+        </div>
+    </section>
+</main>
